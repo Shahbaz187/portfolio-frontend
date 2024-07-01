6153e20df7bbdd0c865c85ae957741a558478efb
shahbazansari8199@gmail.com <shahbazansari8199@gmail.com>
2024-07-01 08:37:05 +0500

    Add client name

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -20,9 +20,17 @@ const fadeInIp = {
 
 const Reviews = () => {
   return (
-    <section class="body-font text-gray-600 lg:overflow-hidden" >
-      <motion.div initial="hidden" animate="show" variants={fadeInIp} class="container mx-auto px-5 py-24">
-        <motion.h1 variants={fadeInIp} class="title-font mb-12 text-center sm:text-4xl md:text-5xl text-3xl font-medium text-gray-900">
+    <section class="body-font text-gray-600 lg:overflow-hidden">
+      <motion.div
+        initial="hidden"
+        animate="show"
+        variants={fadeInIp}
+        class="container mx-auto px-5 py-24"
+      >
+        <motion.h1
+          variants={fadeInIp}
+          class="title-font mb-12 text-center text-3xl font-medium text-gray-900 sm:text-4xl md:text-5xl"
+        >
           Clients Reviews
         </motion.h1>
         <motion.div variants={fadeInIp} class="-m-4 flex flex-wrap">
@@ -76,10 +84,11 @@ const Reviews = () => {
                 <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
               </svg>
               <p class="mb-6 leading-relaxed">
-                 <b>Shahbaz Ansari</b> was excellent to work with. He did an amazing job, was
-                patient and went above and beyond for us. He responded quickly
-                throughout and helped with everything. We would recommend him
-                highly and will 100% be working with him again soon.
+                <b>Shahbaz Ansari</b> was excellent to work with. He did an
+                amazing job, was patient and went above and beyond for us. He
+                responded quickly throughout and helped with everything. We
+                would recommend him highly and will 100% be working with him
+                again soon.
               </p>
               <a class="inline-flex items-center">
                 <img
@@ -88,7 +97,9 @@ const Reviews = () => {
                   class="h-12 w-12 flex-shrink-0 rounded-full object-cover object-center"
                 />
                 <span class="flex flex-grow flex-col pl-4">
-                  <span class="title-font font-medium text-gray-900"></span>
+                  <span class="title-font font-medium text-gray-900">
+                    Anthony D
+                  </span>
                   <span class="flex gap-1 text-sm text-gray-500">
                     <FaStar />
                     <FaStar />
